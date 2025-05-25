e5bc9890bc20d99cfc1bd829411045158259cc0d
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-05-25 16:19:49 -0300

    .

---
+++ poo-exercicios/desafioHeranca.php
@@ -0,0 +1,79 @@
+<?php
+class Cliente
+{
+    public string $logradouro;
+    public string $bairro;
+
+    function __construct(string $logradouro, string $bairro)
+    {
+        $this->logradouro = $logradouro;
+        $this->bairro = $bairro;
+    }
+
+    function verEndereco() : string
+    {
+        return "O cliente se localiza no logradouro: {$this->logradouro}, e bairro: {$this->bairro}";
+    }
+
+}
+
+class ClientePessoaFisica extends Cliente
+{
+    private string $nome;
+    private int $cpf;
+    
+    function __construct(string $nome, int $cpf)
+    {
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+    function verInfomacaoUsuario() : string
+    {
+        return "O cliente {$this->nome}, portador do CPF: {$this->cpf}, localiza-se no seguinte endereço: Logradouro-> {$this->logradouro}; Bairro-> {$this->bairro}";
+    }
+
+    function getNome(){
+        return $this->nome;
+    }
+    function getCpf(){
+        return $this->cpf;
+    }
+}
+class ClientePessoaJurida extends Cliente
+{
+    private int $cnpj;
+    private string $nomeEmpresa;
+
+    function __construct(int $cnpj, string $nomeEmpresa)
+    {
+        $this->cnpj = $cnpj;
+        $this->nomeEmpresa = $nomeEmpresa;
+    }
+
+
+    function verInfomacaoEmpresa() : string
+    {
+        return "A empresa  {$this->nomeEmpresa}, portadora do CNPJ: {$this->cnpj}, localiza-se no seguinte endereço: Logradouro-> {$this->logradouro}; Bairro-> {$this->bairro}";
+    }
+    function getCnpj(){
+        return $this->cnpj;
+    }
+    function getNomeEmpresa(){
+        return $this->nomeEmpresa;
+    }
+}
+
+$cliente = new Cliente("Dom Bosco","São Francisco");
+
+$clientePF = new ClientePessoaFisica("João Silva", 12345678900);
+$clientePJ = new ClientePessoaJurida(12345678901234, "Empresa XYZ");
+
+// Exibindo informações
+echo $clientePF->verInfomacaoUsuario() . "\n";
+echo $clientePJ->verInfomacaoEmpresa() . "\n";
+
+
+
+
+?>
